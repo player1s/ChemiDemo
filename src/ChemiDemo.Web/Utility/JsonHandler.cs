8a16aa50bb51f0ae1b4df0a514bf9474527e169d
Levente Székely <31960469+player1s@users.noreply.github.com>
2019-05-04 02:09:12 +0200

    added json handler

---
+++ src/ChemiDemo.Web/Utility/JsonHandler.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ChemiDemo.Web.Models;
+using Newtonsoft.Json;
+
+namespace ChemiDemo.Web.Utility
+{
+    public class JsonHandler
+    {
+        public List<Product> deSerializeProductsToList(String json)
+        {
+            List<Product> products = new List<Product>();
+
+            products = (List<Product>) JsonConvert.DeserializeObject<List<Product>>(json);
+
+            return products;
+        }
+    }
+}
